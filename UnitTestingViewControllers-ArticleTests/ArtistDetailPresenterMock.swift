abebc85b408ffe664ce43925cce4a42d7ccc702c
V8tr <ne0_n@mail.ru>
2019-11-11 19:10:03 +0200

    Add tests

---
+++ UnitTestingViewControllers-ArticleTests/ArtistDetailPresenterMock.swift
@@ -0,0 +1,25 @@
+//
+//  ArtistDetailPresenterMock.swift
+//  UnitTestingViewControllers-ArticleTests
+//
+//  Created by Vadym Bulavin on 11/11/19.
+//  Copyright © 2019 Vadym Bulavin. All rights reserved.
+//
+
+import Foundation
+@testable import UnitTestingViewControllers_Article
+
+class ArtistDetailPresenterMock: ArtistDetailPresenter {
+    
+    private(set) var onViewLoadedCalled = false
+    
+    func onViewLoaded() {
+        onViewLoadedCalled = true
+    }
+    
+    private(set) var onEditCalled = false
+    
+    func onEdit() {
+        onEditCalled = true
+    }
+}
---
+++ UnitTestingViewControllers-ArticleTests/ArtistDetailViewControllerTests.swift
@@ -0,0 +1,55 @@
+//
+//  ArtistDetailViewControllerTests.swift
+//  UnitTestingViewControllers-ArticleTests
+//
+//  Created by Vadym Bulavin on 11/11/19.
+//  Copyright © 2019 Vadym Bulavin. All rights reserved.
+//
+
+import XCTest
+@testable import UnitTestingViewControllers_Article
+
+class ArtistDetailViewControllerTests: XCTestCase {
+    
+    let presenter = ArtistDetailPresenterMock()
+
+    func testViewDidLoadCallsPresenter() {
+        let sut = makeSUT()
+        
+        sut.viewDidLoad()
+        
+        XCTAssertTrue(presenter.onViewLoadedCalled)
+    }
+
+    func testOnEditCallsPresenter() {
+        let sut = makeSUT()
+        
+        sut.onEdit(.init())
+        
+        XCTAssertTrue(presenter.onEditCalled)
+    }
+
+    func testRender() {
+        let props = ArtistDetailProps(title: "TITLE", fullName: "NAME", numberOfAlbums: "1", numberOfFollowers: "2")
+        
+        let sut = makeSUT()
+        
+        sut.render(props)
+        
+        XCTAssertEqual(sut.navigationItem.title, "TITLE")
+        XCTAssertEqual(sut.fullNameLabel.text, "NAME")
+        XCTAssertEqual(sut.numberOfAlbumsLabel.text, "1")
+        XCTAssertEqual(sut.numberOfFollowersLabel.text, "2")
+    }
+    
+    // MARK: - Helpers
+    
+    func makeSUT() -> ArtistDetailViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let sut = storyboard.instantiateViewController(identifier: "ArtistDetailViewController") as! ArtistDetailViewController
+        sut.presenter = presenter
+        sut.loadViewIfNeeded()
+        return sut
+    }
+
+}
